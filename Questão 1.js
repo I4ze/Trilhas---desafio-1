c45825a25cd88bcde228ef585e5f206669c3c699
Iaze Guilherme Soares <Iaze.guilherme25@gmail.com>
2025-02-11 18:18:18 -0300

    add Questões

---
+++ Questão 1.js
@@ -0,0 +1,4 @@
+//Crie uma variável chamada nome e atribua seu nome a ela. Em seguida, exiba o valor dessa variável.
+let nome;
+nome = "Iaze Guilherme";
+console.log(nome);
---
+++ Questão 2.js
@@ -0,0 +1,8 @@
+//Crie duas variáveis: uma chamada idade e outra chamada altura. Atribua a idade o valor 25 e a altura o valor 1.75. Exiba ambos os valores.
+let idade;
+let altura;
+
+idade = 25;
+altura = 1.75;
+
+console.log(`Idade = ${idade}, Altura = ${altura}`, altura)
